c1ad7ce12ad3b4a01548af0a12a7ce1ca598d8f8
Lisa Ridley <l.ridley@vardot.com>
2025-01-14 16:49:36 -0500

    Fixing FlysystemStreamWrapper tests for additional patched issues on original package

--- tests/FlysystemStreamWrapperTest.php
+++ tests/FlysystemStreamWrapperTest.php
@@ -67,3 +67,19 @@ class FlysystemStreamWrapperTest extends TestCase {
         FlysystemStreamWrapper::unregisterAll();
     }
 }
+
+/**
+ * An alternate version of the local adapter that doesn't support visibility.
+ */
+class NoVisibilityLocal extends Local
+{
+    public function getVisibility($path)
+    {
+        throw new \LogicException();
+    }
+
+    public function setVisibility($path, $visibility)
+    {
+        throw new \LogicException();
+    }
+}
--- tests/WritableOptimzedTest.php
+++ tests/WritableOptimzedTest.php
@@ -19,3 +19,22 @@ class WritableOptimzedTest extends StreamOperationTest
         FlysystemStreamWrapper::register('flysystem', $this->filesystem);
     }
 }
+
+class WritableLocal extends Local
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function readStream($path)
+    {
+        $location = $this->applyPathPrefix($path);
+        $handle = fopen($location, 'r');
+
+        $stream = fopen('php://temp', 'w+b');
+        stream_copy_to_stream($handle, $stream);
+        fclose($handle);
+        fseek($stream, 0);
+
+        return compact('stream', 'path');
+    }
+}
